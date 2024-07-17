9f4508c82d4a7ede20b9fe26ed0218f903f72cc7
paulparkinson <paul.parkinson@gmail.com>
2024-07-17 13:31:32 -0400

    spring boot backend clean up and additions

--- pok-backend-springboot/src/main/java/podsofkon/PodsOfKonController.java
+++ pok-backend-springboot/src/main/java/podsofkon/PodsOfKonController.java
@@ -62,16 +62,12 @@ public class PodsOfKonController {
             @RequestParam(value = "tshirtsize") String tshirtsize,
             @RequestParam(value = "comments") String comments,
             Model model) {
-        if(!player1Name.trim().equals("") && !player2Name.trim().equals("")) {
-            return "<html>Please enter only Player 1 OR Player 2 name (not both).<br><br>" +
-                    "Please return to previous screen to re submit form.  Thank you.</html>";
-        }
         String playerName = "unknown";
-        if (!player1Name.trim().equals("")) {
+        if (player1Name!=null && !player1Name.trim().equals("")) {
             this.player1Name = player1Name;
             playerName = player1Name;
         }
-        if (!player2Name.trim().equals("")) {
+        if (player2Name!=null && !player2Name.trim().equals("")) {
             this.player2Name = player2Name;
             playerName = player2Name;
         }
